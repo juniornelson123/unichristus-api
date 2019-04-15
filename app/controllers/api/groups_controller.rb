473fca3cbff47bb139504e06ddc21e1c2cd291c9
junior nelson <junior123nelson@gmail.com>
2019-04-15 16:09:34 -0300

    base

--- app/controllers/api/groups_controller.rb
+++ app/controllers/api/groups_controller.rb
@@ -30,8 +30,8 @@ class Api::GroupsController < ApplicationController
     @class_room_id = params[:class_room_id]
     @users = User.joins(:class_rooms).where("class_rooms_users.class_room_id in (?) and users.role = ?", @class_room_id, 0)
     
-    @user = User.create(name: "Grupo #{group}", email: "grupo#{group}@email.com", password: "grupo#{group}", role: 2)
-    @new_group = Group.create(name: "Grupo #{group}", description: "Grupo #{group}", user_id: @user.id, class_room_id: @class_room_id)
+    @user = User.create!(name: "Grupo #{group}", email: "grupo#{group}@email.com", password: "grupo#{group}", role: 2)
+    @new_group = Group.create!(name: "Grupo #{group}", description: "Grupo #{group}", user_id: @user.id, class_room_id: @class_room_id)
     
     @users.each_with_index do |user, index|
       count = count + 1
@@ -39,8 +39,8 @@ class Api::GroupsController < ApplicationController
       array_users << user
       if count >= @limit
         count = 0
-        @user = User.create(name: "Grupo #{group}", email: "grupo#{group}@email.com", password: "grupo1", role: 2)
-        @new_group = Group.create(name: "Grupo #{group}", description: "Grupo #{group}", user_id: @user.id, class_room_id: @class_room_id)
+        @user = User.create!(name: "Grupo #{group}", email: "grupo#{group}@email.com", password: "grupo1", role: 2)
+        @new_group = Group.create!(name: "Grupo #{group}", description: "Grupo #{group}", user_id: @user.id, class_room_id: @class_room_id)
       end
     end
     
